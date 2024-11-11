1222d5c26d94cebb5ffd77680a1ce206636d6405
Darshil Shah <sdarshil786@gmail.com>
2024-11-11 22:12:33 +0530

    initial commit for clerk

---
+++ src/app/api/clerk/webhook/route.ts
@@ -0,0 +1,23 @@
+import { db } from "@/server/db";
+
+// /api/clerk/webhook
+export const POST = async (req: Request) => {
+  const { data } = await req.json();
+  //console.log("clerk webhook received", data);
+  const emailAddress = data.email_addresses[0].email_address;
+  const firstName = data.first_name;
+  const lastName = data.last_name;
+  const imageUrl = data.image_url;
+  const id = data.id;
+  await db.user.create({
+    data: {
+      id: id,
+      emailAddress: emailAddress,
+      firstName: firstName,
+      lastName: lastName,
+      imageUrl: imageUrl,
+    },
+  });
+  console.log("user created");
+  return new Response("Webhook received", { status: 200 });
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+import { LatestPost } from "@/app/_components/post";
+import { api, HydrateClient } from "@/trpc/server";
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  return <Button>Hello World</Button>;
+}
